0ac5d5ec74bc7b686d93629ec0027ed14e031762
Bryce Krah <krahbryce@yahoo.com>
2016-02-25 17:27:25 -0500

    initial setup done, switching to branch dev

--- server.js
+++ server.js
@@ -6,12 +6,23 @@ var session          = require('express-session');
 var pgSession        = require('connect-pg-simple')
 var path             = require('path');
 var methodOverride   = require('method-override');
+var env              = require('dotenv');
 var app              = express();
 
+app.use( bodyParser.urlencoded({ extended: false }));
+app.use( bodyParser.json());
+
+app.use( logger('short'));
+
+app.use( methodOverride('_method'));
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
 
 
-
-
+app.get('/', function(req,res){
+  res.send("homepageeee")
+})
 
 
 var port = process.env.PORT || 3000;
